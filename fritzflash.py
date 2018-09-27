94e98fa094cef455c2dfb582795384c376bb901c
David Bauer <mail@david-bauer.net>
2018-09-28 00:22:50 +0200

    fritzflash: add 450E image auto-selection

--- fritzflash.py
+++ fritzflash.py
@@ -148,6 +148,7 @@ def autodiscover_avm_ip():
 def determine_image_name(env_string):
     models = {
         "173": "avm-fritz-wlan-repeater-300e-sysupgrade.bin",
+        "200": "avm-fritz-wlan-repeater-450e-sysupgrade.bin",
         "219": "avm-fritz-box-4020-sysupgrade.bin",
         "227": "avm-fritz-box-4040-bootloader.bin"
     }
