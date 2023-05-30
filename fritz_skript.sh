2612f4310dcdd5726fb5db3f939ba9d1638b0567
Josha Bartsch <>
2023-05-30 23:16:32 +0200

    Added verbosity in messages Fix ssh options for OpenSSH 9

--- fritz_skript.sh
+++ fritz_skript.sh
@@ -1,31 +1,36 @@
-#!/bin/sh
+!/bin/sh
 # usage: fritz_skript.sh eno2
-set -e
+#set -e
 dev="$1"
 
 ip a a 192.168.178.2/24 dev $dev
 
 python3 ./fritzflash.py
 
-sleep 300
-
 ip a d 192.168.178.2/24 dev $dev
-ip a a 192.168.1.2/24 dev $dev
+ip a a 192.168.1.70/24 dev $dev
+
+echo "openwrt should be flashed to RAM"
+echo "did set ip address - wait 30s for reboot"
+sleep 30
+
+echo "reboot should have happened waiting for openwrt in RAM"
 
 while ! ping -c 1 -w 1 192.168.1.1 > /dev/null 2>&1; do
 	echo "wait 1 second"
 	sleep 1
 done
 
-sleep 1
+echo "host online - write firmware to flash"
+sleep 20
 
-SCP="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+SCP="scp -O -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostkeyAlgorithms=+ssh-rsa"
 
 $SCP ./uboot-fritz7530.bin root@192.168.1.1:/tmp
-$SCP ./avm_fritzbox-7530-squashfs-sysupgrade.bin root@192.168.1.1:/tmp
+$SCP ./firmware.bin root@192.168.1.1:/tmp
 
-$SSH -c root@192.168.1.1 "mtd write /tmp/uboot-fritz7530.bin uboot0 && mtd write /tmp/uboot-fritz7530.bin uboot1"
-$SSH -c root@192.168.1.1 "ubirmvol /dev/ubi0 --name=avm_filesys_0 && ubirmvol /dev/ubi0 --name=avm_filesys_1"
-$SSH -c root@192.168.1.1 "sysupgrade -n /tmp/avm_fritzbox-7530-squashfs-sysupgrade.bin
-
+$SSH root@192.168.1.1 "mtd write /tmp/uboot-fritz7530.bin uboot0 && mtd write /tmp/uboot-fritz7530.bin uboot1"
+$SSH root@192.168.1.1 "ubirmvol /dev/ubi0 --name=avm_filesys_0 && ubirmvol /dev/ubi0 --name=avm_filesys_1"
+$SSH root@192.168.1.1 "sysupgrade -n /tmp/firmware.bin"
+echo "success"
