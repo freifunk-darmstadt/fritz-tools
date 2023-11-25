f4c3d0519eadbecfcca46da847f98fa7bc9eec78
userofgithub023897 <37366808+userofgithub023897@users.noreply.github.com>
2023-11-25 23:56:18 +0100

    Update fritzflash.py
    fix: typos

--- fritzflash.py
+++ fritzflash.py
@@ -76,7 +76,7 @@ def start_message(ip_address):
         "You can always find the most current version of this script at https://www.github.com/freifunk-darmstadt/fritz-tools\n\n"
         "It is strongly recommended to only connect your computer to the device you want to flash.\n"
         "Try to disable all other connections (Ethernet, WiFi/WLAN, VMs) if detection fails.\n\n"
-        "Sometimes an unmanaged switch between your AVM device and your computer is helpfull.\n\n"
+        "Sometimes an unmanaged switch between your AVM device and your computer is helpful.\n\n"
         "Before we start, make sure you have assigned your PC a static IP Address in the Subnet of the device you want to flash.\n"
         "The following example would be a completely fine option:\n")
     print("IP-Address: %s" % str(ipaddress.ip_address(ip_address) + 1))
@@ -384,7 +384,7 @@ if __name__ == '__main__':
             input()
             exit(1)
 
-        print("\nAutodiscovery succesful!")
+        print("\nAutodiscovery successful!")
         print("-> Device detected at %s." % ip)
 
     if args.image is None:
